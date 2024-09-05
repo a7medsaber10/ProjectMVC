39eabb97a8393ff66751ba90cf96b31d9c5e3258
a7medsaber10 <ahmedsa2522003@gmail.com>
2024-09-05 13:45:45 +0300

    Data Access Layer

---
+++ ProjectMVC.DAL/Data/Contexts/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectMVC.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMVC.DAL.Data.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .; Database = ProjectMVC; Trusted_Connection = True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
+        }
+    }
+}
