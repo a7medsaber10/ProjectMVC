0ab4643a58c58ef6da0e96a2be6b750bd5131933
a7medsaber10 <ahmedsa2522003@gmail.com>
2024-09-05 14:17:42 +0300

    Repository Design Pattern

---
+++ ProjectMVC.BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,22 @@
+﻿using ProjectMVC.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMVC.BLL.Interfaces
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll();
+
+        Department GetById(int id);
+
+        int Add(Department department);
+
+        int Update(Department department);
+
+        int Delete(int id);
+    }
+}
---
+++ ProjectMVC.BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,38 @@
+﻿using ProjectMVC.BLL.Interfaces;
+using ProjectMVC.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMVC.BLL.Repositories
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        public int Add(Department department)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Department GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(Department department)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
